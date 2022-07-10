619eb522f0dcb9c7a9fd8609b24c76a9d11ca381
jose pimentel <107373532+pecas10@users.noreply.github.com>
2022-07-10 18:11:44 -0300

    subi mi trabajo
    espero pueda corregirme quiero aprender sobre esto, hago lo mejor que puedo.

---
+++ ejercicio_1.1.py
@@ -0,0 +1,39 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con bucles "while"
+
+x = 0
+# Dado el siguiente "while", complete la condicion
+# para que el "while" itere siempre que <x sea menor a 6>
+# Además, complete la línea de código necesaria para que
+# el valor de "x" incremente "1" en cada iteración
+condicion = False
+
+# reemplace "condicion" por lo que crea necesario
+while x < 6:
+    print("Valor de x =", x)
+    x+= 1
+
+print('ya pudo terminar el juego')
+
+    # Coloque la línea de código para que "x" incremente "1"
+
+x = 5
+# Dado el siguiente "while", complete la condicion
+# para que el "while" itere siempre que <x sea mayor o igual a 0>
+# Además, complete la línea de código necesaria para que
+# el valor de "x" decremente "1" en cada iteración
+
+while x >= 0: # reemplace "condicion" por lo que crea necesario
+    print("Valor de x =", x)
+    x-= 1
+    # Coloque la línea de código para que "x" decremente "1"
+
+print("terminamos!")
---
+++ ejercicio_2.2.py
@@ -0,0 +1,29 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejemplos con bucles "for"
+
+# Dado la siguiente lista de colores, utilizar "for"
+# para imprimir en pantalla todos los colores
+colores = ['rojo', 'naranja', 'verde', 'azul']
+
+# Itere el "for" utilizando la lista como parámero
+# y utilizar como elemento del "for" cada color
+# for color ...
+
+for lista in colores:
+    print(lista)
+# Itere el "for" utilizando el tamaño de la lista
+# como parámetro y utilizar el índice para acceder a
+# los elementos de la lista
+# for i ..
+for i in range(len(colores)):
+    print(colores[i])
+
+print("terminamos!")
